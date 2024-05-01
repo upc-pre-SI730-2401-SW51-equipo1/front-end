7fdbcc4b08865d784b8d1117d66ab5268f7c4ae5
Sergio Pecan <u20201a938@upc.edu.pe>
2024-05-01 00:57:25 -0500

    feature(filter): update filter

--- src/recommendations/pages/recomendation.vue
+++ src/recommendations/pages/recomendation.vue
@@ -1,5 +1,5 @@
 <script >
-import { defineComponent } from "vue";
+
 import Header from "@/public/pages/header.vue";
 import Footer from "@/public/pages/footer.vue";
 import Recomendation from "@/recommendations/component/recomendation-content.component.vue";
