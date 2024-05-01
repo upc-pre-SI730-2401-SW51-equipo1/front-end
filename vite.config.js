81d7550c45afc4a8e3b3eff9e0f491e70e6c205e
Sergio Pecan <u20201a938@upc.edu.pe>
2024-05-01 17:09:13 -0500

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
+  base: '/front-end',
   plugins: [
     vue(),
     svgLoader()
